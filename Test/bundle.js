e11397a54ba97bb99d90e224d86facdf50772381
shykai <shykai@gmail.com>
2025-07-07 16:59:46 +0800

    update Test

--- Test/bundle.js
+++ Test/bundle.js
@@ -1037,234 +1037,16 @@ class Player extends _combatUnit__WEBPACK_IMPORTED_MODULE_1__["default"] {
         this.hrid = "player";
     }
 
-    static expTable = {
-        1: 0
-       ,2: 33
-       ,3: 76
-       ,4: 132
-       ,5: 202
-       ,6: 286
-       ,7: 386
-       ,8: 503
-       ,9: 637
-       ,10: 791
-       ,11: 964
-       ,12: 1159
-       ,13: 1377
-       ,14: 1620
-       ,15: 1891
-       ,16: 2192
-       ,17: 2525
-       ,18: 2893
-       ,19: 3300
-       ,20: 3750
-       ,21: 4247
-       ,22: 4795
-       ,23: 5400
-       ,24: 6068
-       ,25: 6805
-       ,26: 7618
-       ,27: 8517
-       ,28: 9508
-       ,29: 10604
-       ,30: 11814
-       ,31: 13151
-       ,32: 14629
-       ,33: 16262
-       ,34: 18068
-       ,35: 20064
-       ,36: 22271
-       ,37: 24712
-       ,38: 27411
-       ,39: 30396
-       ,40: 33697
-       ,41: 37346
-       ,42: 41381
-       ,43: 45842
-       ,44: 50773
-       ,45: 56222
-       ,46: 62243
-       ,47: 68895
-       ,48: 76242
-       ,49: 84355
-       ,50: 93311
-       ,51: 103195
-       ,52: 114100
-       ,53: 126127
-       ,54: 139390
-       ,55: 154009
-       ,56: 170118
-       ,57: 187863
-       ,58: 207403
-       ,59: 228914
-       ,60: 252584
-       ,61: 278623
-       ,62: 307256
-       ,63: 338731
-       ,64: 373318
-       ,65: 411311
-       ,66: 453030
-       ,67: 498824
-       ,68: 549074
-       ,69: 604193
-       ,70: 664632
-       ,71: 730881
-       ,72: 803472
-       ,73: 882985
-       ,74: 970050
-       ,75: 1065351
-       ,76: 1169633
-       ,77: 1283701
-       ,78: 1408433
-       ,79: 1544780
-       ,80: 1693774
-       ,81: 1856536
-       ,82: 2034279
-       ,83: 2228321
-       ,84: 2440088
-       ,85: 2671127
-       ,86: 2923113
-       ,87: 3197861
-       ,88: 3497335
-       ,89: 3823663
-       ,90: 4179145
-       ,91: 4566274
-       ,92: 4987741
-       ,93: 5446463
-       ,94: 5945587
-       ,95: 6488521
-       ,96: 7078945
-       ,97: 7720834
-       ,98: 8418485
-       ,99: 9176537
-       ,100: 10000000
-       ,101: 11404976
-       ,102: 12904567
-       ,103: 14514400
-       ,104: 16242080
-       ,105: 18095702
-       ,106: 20083886
-       ,107: 22215808
-       ,108: 24501230
-       ,109: 26950540
-       ,110: 29574787
-       ,111: 32385721
-       ,112: 35395838
-       ,113: 38618420
-       ,114: 42067584
-       ,115: 45758332
-       ,116: 49706603
-       ,117: 53929328
-       ,118: 58444489
-       ,119: 63271179
-       ,120: 68429670
-       ,121: 73941479
-       ,122: 79829440
-       ,123: 86117783
-       ,124: 92832214
-       ,125: 100000000
-       ,126: 114406130
-       ,127: 130118394
-       ,128: 147319656
-       ,129: 166147618
-       ,130: 186752428
-       ,131: 209297771
-       ,132: 233962072
-       ,133: 260939787
-       ,134: 290442814
-       ,135: 322702028
-       ,136: 357968938
-       ,137: 396517495
-       ,138: 438646053
-       ,139: 484679494
-       ,140: 534971538
-       ,141: 589907252
-       ,142: 649905763
-       ,143: 715423218
-       ,144: 786955977
-       ,145: 865044093
-       ,146: 950275074
-       ,147: 1043287971
-       ,148: 1144777804
-       ,149: 1255500373
-       ,150: 1376277458
-       ,151: 1508002470
-       ,152: 1651646566
-       ,153: 1808265285
-       ,154: 1979005730
-       ,155: 2165114358
-       ,156: 2367945418
-       ,157: 2588970089
-       ,158: 2829786381
-       ,159: 3092129857
-       ,160: 3377885250
-       ,161: 3689099031
-       ,162: 4027993033
-       ,163: 4396979184
-       ,164: 4798675471
-       ,165: 5235923207
-       ,166: 5711805728
-       ,167: 6229668624
-       ,168: 6793141628
-       ,169: 7406162301
-       ,170: 8073001662
-       ,171: 8798291902
-       ,172: 9587056372
-       ,173: 10444742007
-       ,174: 11377254401
-       ,175: 12390995728
-       ,176: 13492905745
-       ,177: 14690506120
-       ,178: 15991948361
-       ,179: 17406065609
-       ,180: 18942428633
-       ,181: 20611406335
-       ,182: 22424231139
-       ,183: 24393069640
-       ,184: 26531098945
-       ,185: 28852589138
-       ,186: 31372992363
-       ,187: 34109039054
-       ,188: 37078841860
-       ,189: 40302007875
-       ,190: 43799759843
-       ,191: 47595067021
-       ,192: 51712786465
-       ,193: 56179815564
-       ,194: 61025256696
-       ,195: 66280594953
-       ,196: 71979889960
-       ,197: 78159982881
-       ,198: 84860719814
-       ,199: 92125192822
-       ,200: 100000000000
-   };
-
     static createFromDTO(dto) {
         let player = new Player();
 
         player.staminaLevel = dto.staminaLevel;
         player.intelligenceLevel = dto.intelligenceLevel;
-        // player.attackLevel = dto.attackLevel; 使用映射后的攻击等级
+        player.attackLevel = dto.attackLevel;
         player.powerLevel = dto.powerLevel;
         player.defenseLevel = dto.defenseLevel;
         player.rangedLevel = dto.rangedLevel;
         player.magicLevel = dto.magicLevel;
-
-        let attackExp = this.expTable[dto.attackLevel];
-        if (player.rangedLevel > 1){
-            attackExp += (this.expTable[player.rangedLevel] + this.expTable[player.rangedLevel+1])/2*0.1;
-        }
-        if (player.magicLevel > 1){
-            attackExp += (this.expTable[player.magicLevel] + this.expTable[player.magicLevel+1])/2*0.1;
-        }
-        
-        for (const [key, value] of Object.entries(this.expTable)) {
-            if (attackExp >= value){
-                player.attackLevel = Number(key);
-            }
-        }
-        console.log(dto.hrid, 'attackLevel before:', dto.attackLevel, 'after:', player.attackLevel);
 
         player.hrid = dto.hrid;
 
@@ -2316,6 +2098,242 @@ function updateLevels() {
         let levelInput = document.getElementById("inputLevel_" + skill);
         player[skill + "Level"] = Number(levelInput.value);
     });
+    updateReAttackLevel();
+}
+
+function calcAttackLevel(attackLevel, rangedLevel, magicLevel) {
+    const expTable = {
+        1: 0
+        ,2: 33
+        ,3: 76
+        ,4: 132
+        ,5: 202
+        ,6: 286
+        ,7: 386
+        ,8: 503
+        ,9: 637
+        ,10: 791
+        ,11: 964
+        ,12: 1159
+        ,13: 1377
+        ,14: 1620
+        ,15: 1891
+        ,16: 2192
+        ,17: 2525
+        ,18: 2893
+        ,19: 3300
+        ,20: 3750
+        ,21: 4247
+        ,22: 4795
+        ,23: 5400
+        ,24: 6068
+        ,25: 6805
+        ,26: 7618
+        ,27: 8517
+        ,28: 9508
+        ,29: 10604
+        ,30: 11814
+        ,31: 13151
+        ,32: 14629
+        ,33: 16262
+        ,34: 18068
+        ,35: 20064
+        ,36: 22271
+        ,37: 24712
+        ,38: 27411
+        ,39: 30396
+        ,40: 33697
+        ,41: 37346
+        ,42: 41381
+        ,43: 45842
+        ,44: 50773
+        ,45: 56222
+        ,46: 62243
+        ,47: 68895
+        ,48: 76242
+        ,49: 84355
+        ,50: 93311
+        ,51: 103195
+        ,52: 114100
+        ,53: 126127
+        ,54: 139390
+        ,55: 154009
+        ,56: 170118
+        ,57: 187863
+        ,58: 207403
+        ,59: 228914
+        ,60: 252584
+        ,61: 278623
+        ,62: 307256
+        ,63: 338731
+        ,64: 373318
+        ,65: 411311
+        ,66: 453030
+        ,67: 498824
+        ,68: 549074
+        ,69: 604193
+        ,70: 664632
+        ,71: 730881
+        ,72: 803472
+        ,73: 882985
+        ,74: 970050
+        ,75: 1065351
+        ,76: 1169633
+        ,77: 1283701
+        ,78: 1408433
+        ,79: 1544780
+        ,80: 1693774
+        ,81: 1856536
+        ,82: 2034279
+        ,83: 2228321
+        ,84: 2440088
+        ,85: 2671127
+        ,86: 2923113
+        ,87: 3197861
+        ,88: 3497335
+        ,89: 3823663
+        ,90: 4179145
+        ,91: 4566274
+        ,92: 4987741
+        ,93: 5446463
+        ,94: 5945587
+        ,95: 6488521
+        ,96: 7078945
+        ,97: 7720834
+        ,98: 8418485
+        ,99: 9176537
+        ,100: 10000000
+        ,101: 11404976
+        ,102: 12904567
+        ,103: 14514400
+        ,104: 16242080
+        ,105: 18095702
+        ,106: 20083886
+        ,107: 22215808
+        ,108: 24501230
+        ,109: 26950540
+        ,110: 29574787
+        ,111: 32385721
+        ,112: 35395838
+        ,113: 38618420
+        ,114: 42067584
+        ,115: 45758332
+        ,116: 49706603
+        ,117: 53929328
+        ,118: 58444489
+        ,119: 63271179
+        ,120: 68429670
+        ,121: 73941479
+        ,122: 79829440
+        ,123: 86117783
+        ,124: 92832214
+        ,125: 100000000
+        ,126: 114406130
+        ,127: 130118394
+        ,128: 147319656
+        ,129: 166147618
+        ,130: 186752428
+        ,131: 209297771
+        ,132: 233962072
+        ,133: 260939787
+        ,134: 290442814
+        ,135: 322702028
+        ,136: 357968938
+        ,137: 396517495
+        ,138: 438646053
+        ,139: 484679494
+        ,140: 534971538
+        ,141: 589907252
+        ,142: 649905763
+        ,143: 715423218
+        ,144: 786955977
+        ,145: 865044093
+        ,146: 950275074
+        ,147: 1043287971
+        ,148: 1144777804
+        ,149: 1255500373
+        ,150: 1376277458
+        ,151: 1508002470
+        ,152: 1651646566
+        ,153: 1808265285
+        ,154: 1979005730
+        ,155: 2165114358
+        ,156: 2367945418
+        ,157: 2588970089
+        ,158: 2829786381
+        ,159: 3092129857
+        ,160: 3377885250
+        ,161: 3689099031
+        ,162: 4027993033
+        ,163: 4396979184
+        ,164: 4798675471
+        ,165: 5235923207
+        ,166: 5711805728
+        ,167: 6229668624
+        ,168: 6793141628
+        ,169: 7406162301
+        ,170: 8073001662
+        ,171: 8798291902
+        ,172: 9587056372
+        ,173: 10444742007
+        ,174: 11377254401
+        ,175: 12390995728
+        ,176: 13492905745
+        ,177: 14690506120
+        ,178: 15991948361
+        ,179: 17406065609
+        ,180: 18942428633
+        ,181: 20611406335
+        ,182: 22424231139
+        ,183: 24393069640
+        ,184: 26531098945
+        ,185: 28852589138
+        ,186: 31372992363
+        ,187: 34109039054
+        ,188: 37078841860
+        ,189: 40302007875
+        ,190: 43799759843
+        ,191: 47595067021
+        ,192: 51712786465
+        ,193: 56179815564
+        ,194: 61025256696
+        ,195: 66280594953
+        ,196: 71979889960
+        ,197: 78159982881
+        ,198: 84860719814
+        ,199: 92125192822
+        ,200: 100000000000
+    };
+
+    let attackExp = (expTable[attackLevel] + expTable[attackLevel + 1]) / 2;
+
+    if (rangedLevel > 1){
+        attackExp += (expTable[rangedLevel] + expTable[rangedLevel+1])/2*0.1;
+    }
+
+    if (magicLevel > 1){
+        attackExp += (expTable[magicLevel] + expTable[magicLevel+1])/2*0.1;
+    }
+    
+    let reAttackLevel = attackLevel;
+    for (const [key, value] of Object.entries(expTable)) {
+        if (attackExp >= value){
+            reAttackLevel = Number(key);
+        }
+    }
+
+    return reAttackLevel;
+}
+
+function updateReAttackLevel() {
+    let attackLevel = Number(document.getElementById("inputLevel_attack").value);
+    let rangedLevel = Number(document.getElementById("inputLevel_ranged").value);
+    let magicLevel = Number(document.getElementById("inputLevel_magic").value);
+
+    let reAttackLevel = calcAttackLevel(attackLevel, rangedLevel, magicLevel);
+
+    let spanReAttackLevel = document.getElementById("reLevel_attack");
+    spanReAttackLevel.innerHTML = reAttackLevel;
 }
 
 // #endregion
@@ -2746,17 +2764,9 @@ function initZones() {
         .sort((a, b) => a.sortIndex - b.sortIndex);
 
     for (const zone of Object.values(gameZones)) {
-        for(let i = 0; i <= zone.maxDifficulty; i++) {
-            let opt = new Option("", zone.hrid + "#" + i);
-            let dungeonName = createElement("span", "dungeonName");
-            dungeonName.setAttribute("data-i18n", "actionNames." + zone.hrid);
-            opt.appendChild(dungeonName);
-
-            let dungeonTier = createElement("span", "dungeonTier");
-            dungeonTier.textContent = '#T' + i;
-            opt.appendChild(dungeonTier);
-            zoneSelect.add(opt);
-        }
+        let opt = new Option(zone.name, zone.hrid);
+        opt.setAttribute("data-i18n", "actionNames." + zone.hrid);
+        zoneSelect.add(opt);
     }
 }
 
@@ -2768,17 +2778,9 @@ function initDungeons() {
         .sort((a, b) => a.sortIndex - b.sortIndex);
 
     for (const dungeon of Object.values(gameDungeons)) {
-        for(let i = 0; i <= dungeon.maxDifficulty; i++) {
-            let opt = new Option("", dungeon.hrid+'#'+i);
-            let dungeonName = createElement("span", "dungeonName");
-            dungeonName.setAttribute("data-i18n", "actionNames." + dungeon.hrid);
-            opt.appendChild(dungeonName);
-
-            let dungeonTier = createElement("span", "dungeonTier");
-            dungeonTier.textContent = '#T' + i;
-            opt.appendChild(dungeonTier);
-            dungeonSelect.add(opt);
-        }
+        let opt = new Option(dungeon.name, dungeon.hrid);
+        opt.setAttribute("data-i18n", "actionNames." + dungeon.hrid);
+        dungeonSelect.add(opt);
     }
 }
 
@@ -4083,13 +4085,26 @@ document.addEventListener('DOMContentLoaded', function () {
         boxes.forEach((checkBox) => { checkBox.checked = isCheck });
     }
 
+    function updateDifficultySelect(isCheck) {
+        const difficultySelect = document.getElementById('selectDifficulty');
+        // disable last four option
+        if (isCheck && Number(difficultySelect.value) >= 3) {
+            difficultySelect.value = 0;
+        }
+        for (let i = 3; i < difficultySelect.options.length; i++) {
+            difficultySelect.options[i].disabled = isCheck;
+        }
+    }
+
     simDungeonToggle.addEventListener('change', function () {
         if (simDungeonToggle.checked) {
             addPlayers();
             updatePlayersCheckbox(true);
+            updateDifficultySelect(true);
         } else {
             removePlayers();
             updatePlayersCheckbox(false);
+            updateDifficultySelect(false);
         }
         updatePlayerNames();
     });
@@ -4188,18 +4203,30 @@ function startSimulation(selectedPlayers) {
     updateNextPlayer(currentPlayerTabId);
     updateState();
     updateUI();
+
+    let useAttackInRework = document.getElementById("attackRework");
+    if (useAttackInRework.checked)
+    {
+        for(let player of playersToSim)
+        {
+            let attackBefore = player.attackLevel;
+            player.attackLevel = calcAttackLevel(attackBefore, player.rangedLevel, player.magicLevel);
+            console.log("player " + player.hrid + " attack level before: " + attackBefore + " after: " + player.attackLevel);
+        }
+    }
+
     let simAllZonesToggle = document.getElementById("simAllToggle");
     let simDungeonToggle = document.getElementById("simDungeonToggle");
     let zoneSelect = document.getElementById("selectZone");
     let dungeonSelect = document.getElementById("selectDungeon");
+    let difficultySelect = document.getElementById("selectDifficulty");
     let simulationTimeInput = document.getElementById("inputSimulationTime");
     let simulationTimeLimit = Number(simulationTimeInput.value) * ONE_HOUR;
     if (!simAllZonesToggle.checked) {
-        let zoneHrid = zoneSelect.value.split('#')[0];
-        let difficultyTier = Number(zoneSelect.value.split('#')[1]);
+        let zoneHrid = zoneSelect.value;
+        let difficultyTier = Number(difficultySelect.value);
         if (simDungeonToggle.checked) {
-            zoneHrid = dungeonSelect.value.split('#')[0];
-            difficultyTier = Number(dungeonSelect.value.split('#')[1]);
+            zoneHrid = dungeonSelect.value;
         }
         let workerMessage = {
             type: "start_simulation",
@@ -4416,6 +4443,7 @@ function loadEquipmentSetIntoUI(equipmentSet) {
         let levelInput = document.getElementById("inputLevel_" + skill);
         levelInput.value = equipmentSet.levels[skill] ?? 1;
     });
+    updateReAttackLevel();
 
     ["head", "body", "legs", "feet", "hands", "weapon", "off_hand", "pouch", "neck", "earrings", "ring", "back"].forEach((type) => {
         let equipmentSelect = document.getElementById("selectEquipment_" + type);
@@ -4634,6 +4662,7 @@ function doSoloImport() {
         let levelInput = document.getElementById("inputLevel_" + skill);
         levelInput.value = importSet.player[skill + "Level"];
     });
+    updateReAttackLevel();
 
     ["head", "body", "legs", "feet", "hands", "off_hand", "pouch", "neck", "earrings", "ring", "back"].forEach((type) => {
         let equipmentSelect = document.getElementById("selectEquipment_" + type);
@@ -4792,6 +4821,7 @@ function updateNextPlayer(currentPlayerNumber) {
         let levelInput = document.getElementById("inputLevel_" + skill);
         levelInput.value = importSet.player[skill + "Level"];
     });
+    updateReAttackLevel();
 
     ["head", "body", "legs", "feet", "hands", "off_hand", "pouch", "neck", "earrings", "ring", "back"].forEach((type) => {
         let equipmentSelect = document.getElementById("selectEquipment_" + type);
