79bdd21c22527f917c434e2168c8f34c8a43bf58
shykai <shykai@gmail.com>
2025-07-10 15:32:21 +0800

    Prevent repeated targeting of same unit in penetrating ability casted by enemy

--- src/combatsimulator/combatSimulator.js
+++ src/combatsimulator/combatSimulator.js
@@ -982,6 +982,8 @@ class CombatSimulator extends EventTarget {
             return;
         }
 
+        let avoidTarget = []
+
         for (let target of targets.filter((unit) => unit && unit.combatDetails.currentHitpoints > 0)) {
             if (target.combatDetails.combatStats.parry > Math.random()) {
                 let tempTarget = source;
@@ -1026,7 +1028,7 @@ class CombatSimulator extends EventTarget {
                     }
                 }
             } else {
-                targets = targets.filter((unit) => unit && unit.combatDetails.currentHitpoints > 0);
+                targets = targets.filter((unit) => unit && !avoidTarget.includes(unit.hrid) && unit.combatDetails.currentHitpoints > 0);
                 if (!source.isPlayer && targets.length > 1 && abilityEffect.targetType == "enemy") {
                     let cumulativeThreat = 0;
                     let cumulativeRanges = [];
@@ -1041,6 +1043,7 @@ class CombatSimulator extends EventTarget {
                     });
                     let randomValueHit = Math.random() * cumulativeThreat;
                     target = cumulativeRanges.find(range => randomValueHit >= range.rangeStart && randomValueHit < range.rangeEnd).player;
+                    avoidTarget.push(target.hrid);
                 }
 
                 let attackResult = CombatUtilities.processAttack(source, target, abilityEffect);
